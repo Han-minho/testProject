945c5a4bc152178dccbb84bb482654454c778838
Hanminho <hihi6024@gmail.com>
2023-06-27 17:31:48 +0900

    오타수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.aps.BlogConfig',
+    'blog.apps.BlogConfig',
     'bookmark.apps.BookmarkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
