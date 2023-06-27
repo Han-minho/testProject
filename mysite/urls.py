7cfd4308bac560c7f02642c5062bc12f50b75be7
Hanminho <hihi6024@gmail.com>
2023-06-27 17:04:17 +0900

    오타 수정(2)

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from bookmark.views import BookmarkLV, BookmarkDV
 from mysite.views import HomeView
 
 urlpatterns = [
