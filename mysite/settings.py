cabf13a0986d3022fa48db6854f4f964502daaa3
Hanminho <hihi6024@gmail.com>
2023-07-03 10:22:19 +0900

    photo app 삭제로 파일 재생성

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'bookmark.apps.BookmarkConfig',
+    'photo.apps.PhotoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
