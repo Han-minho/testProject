afaada23b38566c896fc50a296f6b8ed9c2bc977
Hanminho <hihi6024@gmail.com>
2023-07-06 13:28:04 +0900

    url 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,16 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from mysite.views import HomeView
+from bookmark.views import BookmarkLV, BookmarkDV
+from mysite.views import HomeView, UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('',HomeView.as_view(),name='home'),
-    path('blog/',include('blog.urls')),
-    path('photo/',include('photo.urls')),
-    path('bookmark/', include('bookmark.urls')),
+    path("admin/", admin.site.urls),
+    #인증
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
 
-]
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    path('photo/', include('photo.urls')),
+
+    path('bookmark/', include('bookmark.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
