b3a2cb3de3b2e4183df7737f712291b6d65f6464
Hanminho <hihi6024@gmail.com>
2023-07-06 10:33:45 +0900

    20230706 views.py Album views 로직 추가 (작업중)

--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from photo.models import Album, Photo
 from mysite.views import OwnerOnlyMixin
+from photo.form import PhotoInlineFormSet
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class AlbumLV(ListView):
@@ -20,7 +23,7 @@ class PhotoDV(DetailView):
 class PhotoCV(LoginRequiredMixin,CreateView):
     model = Photo
     fields = ('album','title','image','description')
-    succes_url = reverse_lazy('photo:index')
+    success_url = reverse_lazy('photo:index')
 
 
 class PhotoChangeLV(LoginRequiredMixin,ListView):
@@ -53,4 +56,31 @@ class AlbumDelV(OwnerOnlyMixin, DeleteView):
     model = Album
     success_url = reverse_lazy('photo:index')
 
+
+class AlbumPhotoCV(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ('name', 'description')
+    success_url = reverse_lazy('photo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['formset'] = PhotoInlineFormSet()
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context['formset']
+        for photoform in formset:
+            photoform.instance.owner = self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
 
